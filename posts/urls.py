7f24f2d40a2b45792fa5db39eead42d5c3147cf8
ericksoftware <erickcodessoftware@gmail.com>
2025-05-29 17:08:52 -0700

    Assignment 3

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,10 @@
-import django.urls as path
+from django.urls import path
 from posts import views
-
-
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='list'),
     path('new/', views.PostCreateView.as_view(), name='new'),
     path('<int:pk>/', views.PostDetailView.as_view(), name='detail'),
 ]
+
 
